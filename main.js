798f28317503a3864c7eaaad1b0525ef2c6d4a6b
Dam450 <68165752+dam450@users.noreply.github.com>
2022-04-19 15:39:56 -0300

    added code to draw X and O
    adicionado a lógica para desenhar simbolos dos
    jogadores e para não haver sobreposição dos
    simbolos.

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
-function chooseSquare() {
-  console.log('clicou no quadrado')
+var player = null;
+
+var playerTurn = document.getElementById('player-turn');
+
+changePlayer('X')
+
+function chooseSquare(id) {
+  console.log(`clicou no quadrado ${id}`);
+  var square = document.getElementById(id);
+
+  if (square.innerHTML !== '-') return;
+
+  square.innerHTML = player;
+
+  if (player === 'X') {
+    square.style.color = 'blue'
+    player = 'O';
+  } else {
+    square.style.color = 'red'
+    player = 'X';
+  }
+  changePlayer(player);
+
+}
+
+function changePlayer(value) {
+  player = value;
+  playerTurn.innerHTML = player
+
 }
